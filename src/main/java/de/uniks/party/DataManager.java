a93f0c696cd8213e1de6187336f668722f8fdb83
zuendorf <zuendorf@uni-kassel.de>
2018-12-31 16:42:18 +0100

    event logging refactored

--- src/main/java/de/uniks/party/DataManager.java
+++ src/main/java/de/uniks/party/DataManager.java
@@ -25,6 +25,7 @@ public class DataManager
    public static final String HISTORY_KEY = "historyKey";
 
    private YamlIdMap yamlIdMap;
+   private EventYamler eventYamler;
    private ReflectorMap reflectorMap;
    private File logFilePath = null;
    private File modelFile = null;
@@ -44,6 +45,7 @@ public class DataManager
 
       String packageName = rootObject.getClass().getPackage().getName();
       yamlIdMap = new YamlIdMap(packageName);
+      eventYamler = new EventYamler(packageName).setYamlIdMap(yamlIdMap);
       reflectorMap = new ReflectorMap(packageName);
 
       loadModel(rootObject);
@@ -113,86 +115,7 @@ public class DataManager
 
    private void handleEvent(PropertyChangeEvent e)
    {
-      Object source = e.getSource();
-      StringBuilder buf = new StringBuilder("- ");
-
-      long timeMillis = System.currentTimeMillis();
-      Date date = new Date(timeMillis);
-      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-      String timeString = dateFormat.format(date);
-      buf.append(TIME + ": ").append(timeString).append("\n");
-
-      String sourceKey = yamlIdMap.getOrCreateKey(source);
-      buf.append("  " + SOURCE + ": ").append(sourceKey).append("\n");
-
-      String className = source.getClass().getSimpleName();
-      buf.append("  " + SOURCE_TYPE + ": ").append(className).append("\n");
-
-      String prop = e.getPropertyName();
-      buf.append("  " + PROPERTY + ": ").append(prop).append("\n");
-
-      String historyKey = sourceKey + "/" + prop;
-
-
-      Object oldValue = e.getOldValue();
-      if (oldValue != null)
-      {
-         Class valueClass = oldValue.getClass();
-
-         if (valueClass == String.class)
-         {
-            String encapsulted = Yamler.encapsulate((String) oldValue);
-            buf.append("  " + OLD_VALUE + ": ").append(encapsulted).append("\n");
-         }
-         else if (  valueClass.getName().startsWith("java.lang."))
-         {
-            buf.append("  " + OLD_VALUE + ": ").append(oldValue).append("\n");
-         }
-         else
-         {
-            String valueKey = yamlIdMap.getOrCreateKey(oldValue);
-            buf.append("  " + OLD_VALUE + ": ").append(valueKey).append("\n");
-
-            historyKey += "/" + valueKey;
-
-            className = oldValue.getClass().getSimpleName();
-            buf.append("  " + OLD_VALUE_TYPE + ": ").append(className).append("\n");
-         }
-      }
-
-      Object newValue = e.getNewValue();
-      if (newValue != null)
-      {
-         Class valueClass = newValue.getClass();
-
-         if (valueClass == String.class)
-         {
-            String encapsulted = Yamler.encapsulate((String) newValue);
-            buf.append("  " + NEW_VALUE + ": ").append(encapsulted).append("\n");
-         }
-         else if (  valueClass.getName().startsWith("java.lang."))
-         {
-            buf.append("  " + NEW_VALUE + ": ").append(newValue).append("\n");
-         }
-         else
-         {
-            String valueKey = yamlIdMap.getOrCreateKey(newValue);
-            buf.append("  " + NEW_VALUE + ": ").append(valueKey).append("\n");
-
-            Reflector reflector = reflectorMap.getReflector(className);
-            Object attrValue = reflector.getValue(source, prop);
-            if (attrValue != null && Collection.class.isAssignableFrom(attrValue.getClass()))
-            {
-               historyKey += "/" + valueKey;
-            }
-
-            className = newValue.getClass().getSimpleName();
-            buf.append("  " + NEW_VALUE_TYPE + ": ").append(className).append("\n");
-         }
-      }
-
-      buf.append("  " + HISTORY_KEY + ": ").append(historyKey).append("\n");
-      buf.append("\n");
+      String buf = eventYamler.encode(e);
 
       try
       {
@@ -221,7 +144,7 @@ public class DataManager
                logFilePath.createNewFile();
             }
             FileWriter fileWriter = new FileWriter(logFilePath, true);
-            fileWriter.write(buf.toString());
+            fileWriter.write(buf);
             fileWriter.flush();
             fileWriter.close();
          }
@@ -232,6 +155,8 @@ public class DataManager
          Logger.getGlobal().log(Level.SEVERE, "could not write log to " + logFilePath, e);
       }
    }
+
+
 
 
    private boolean storeModel(Object rootObject)
@@ -325,65 +250,8 @@ public class DataManager
       String content = new String(bytes);
 
       String packageName = rootObject.getClass().getPackage().getName();
-      Yamler yamler = new Yamler();
-      ArrayList<LinkedHashMap<String, String>> list = yamler.decodeList(content);
-
-      String firstKey = null;
-      for (LinkedHashMap<String, String> map : list)
-      {
-         // execute change
-         String sourceKey = map.get(DataManager.SOURCE);
-
-         if (firstKey == null)
-         {
-            firstKey = sourceKey;
-            Object oldObject = yamlIdMap.getObject(firstKey);
-            if (oldObject == null)
-            {
-               yamlIdMap.putNameObject(firstKey, rootObject);
-            }
-         }
-
-         Object sourceObject = yamlIdMap.getObject(sourceKey);
-         String className = map.get(DataManager.SOURCE_TYPE);
-         Reflector reflector = reflectorMap.getReflector(className);
-
-         if (reflector == null)
-         {
-            Logger.getGlobal().log(Level.SEVERE, "did not find a reflector for " + className);
-         }
-
-         if (sourceObject == null)
-         {
-            sourceObject = reflector.newInstance();
-            yamlIdMap.putNameObject(sourceKey, sourceObject);
-         }
-
-         String property = map.get(DataManager.PROPERTY);
-         String newValue = map.get(DataManager.NEW_VALUE);
-         String newValueType = map.get(DataManager.NEW_VALUE_TYPE);
-
-         if (newValueType == null)
-         {
-            reflector.setValue(sourceObject, property, newValue, null);
-         }
-         else
-         {
-            Object newValueObject = yamlIdMap.getObject(newValue);
-            if (newValueObject == null)
-            {
-               Reflector newValueReflector = reflectorMap.getReflector(newValueType);
-               newValueObject = newValueReflector.newInstance();
-               yamlIdMap.putNameObject(newValue, newValueObject);
-            }
-
-            reflector.setValue(sourceObject, property, newValueObject, null);
-         }
-      }
-
-      Object firstObject = yamlIdMap.getObject(firstKey);
+      eventYamler.decode(rootObject, content);
    }
-
 
 
    public byte[] read(File file) {
